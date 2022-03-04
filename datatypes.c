ef181949b7df450300d229aac6b4f2ca23e256cc
Praveenkumar <100259959+praveenlr01@users.noreply.github.com>
2022-03-04 14:57:55 +0530

    to_list_basic_5_datatypes_c

---
+++ datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int i= 1;
+    printf("The value of i =  %i\n",i);
+    float j=2;
+    printf("The value of j = %f\n",j);
+    char k='c';
+    printf("The value of k = %c\n",k);
+    double l=2.3445;
+    printf("The value of l = %lf\n",l);
+    unsigned int m=10;
+    printf("The value of i = %u\n",m);
+}
