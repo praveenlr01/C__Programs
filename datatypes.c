93569e9e20ae8ed6ff412d8f69f54e1548df4aef
Praveenkumar <100259959+praveenlr01@users.noreply.github.com>
2022-03-04 15:05:49 +0530

    to_list_5_basic _datatypes

--- datatypes.c
+++ datatypes.c
@@ -4,11 +4,13 @@ void main()
     int i= 1;
     printf("The value of i =  %i\n",i);
     float j=2;
-    printf("The value of j = %f\n",j);
+    float jj=2.03;
+    printf("The value of j = %f \t The value of jj = %f \n",j,jj);
     char k='c';
     printf("The value of k = %c\n",k);
-    double l=2.3445;
-    printf("The value of l = %lf\n",l);
-    unsigned int m=10;
-    printf("The value of i = %u\n",m);
+    double l=2.344e5;
+    double m=2.0944;
+    printf("The value of l = %lf \t The value of m = %lf\n",l,m);
+    unsigned int n=0;
+    printf("The value of i = %u\n",n);
 }
